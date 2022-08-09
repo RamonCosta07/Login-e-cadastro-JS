8b4431f87e6b7fbce8fa9b678084265b43ed2ae1
RamonCosta07 <ramonnar20@hotmail.com>
2022-08-09 02:41:04 -0300

    Corrigido bug que permitia logar sem login e senha, de forma que agora o botão só aparece após o usuário digitar algo no user

--- js/login.js
+++ js/login.js
@@ -1,41 +1,52 @@
+usuario = document.querySelector("#usuario");
+botao = document.querySelector("#botao");
+
+usuario.addEventListener("keyup", () => {
+  if (usuario.value.length > 0) {
+    botao.setAttribute('style', 'display: block');
+  } else {
+    botao.setAttribute('style', 'display: none');
+  }
+});
+
 function entrar() {
-    let usuario = document.querySelector('#usuario');
-    let userLabel = document.querySelector('#userLabel');
+  let usuario = document.querySelector("#usuario");
+  let userLabel = document.querySelector("#userLabel");
 
-    let senha = document.querySelector('#password');
-    let passwordLabel = document.querySelector('#passwordLabel');
+  let senha = document.querySelector("#password");
+  let passwordLabel = document.querySelector("#passwordLabel");
 
-    let msgError = document.querySelector('#msgError');
-    let listaUser = [];
+  let msgError = document.querySelector("#msgError");
+  let listaUser = [];
 
-    let userValid = {
-        nome: '',
-        user: '',
-        senha: ''
+  let userValid = {
+    nome: "",
+    user: "",
+    senha: "",
+  };
+
+  listaUser = JSON.parse(localStorage.getItem("listaUser"));
+  listaUser.forEach((item) => {
+    if (usuario.value == item.usuarioCad && senha.value == item.senhaCad) {
+      userValid = {
+        nome: item.nomeCad,
+        user: item.usuarioCad,
+        senha: item.senhaCad,
+      };
     }
-
-    listaUser = JSON.parse(localStorage.getItem('listaUser'));
-    listaUser.forEach((item) => {
-        if (usuario.value == item.usuarioCad && senha.value == item.senhaCad) {
-            userValid = {
-                nome: item.nomeCad,
-                user: item.usuarioCad,
-                senha: item.senhaCad
-            }
-        }
-    })
-    if (usuario.value == userValid.user && senha.value == userValid.senha) {
-        window.location.href = '../pages/perfilUsuario.html';
-        let token = Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2);
-        localStorage.setItem('token', token);
-        localStorage.setItem('userLogado', JSON.stringify(userValid));
-    } else {
-        userLabel.setAttribute('style', 'color: red');
-        usuario.setAttribute('style', 'border-color: red');
-        senha.setAttribute('style', 'border-color: red');
-        passwordLabel.setAttribute('style', 'color: red');
-        msgError.setAttribute('style', 'display: block');
-        msgError.innerHTML = 'Usuário ou senha incorretos';
-        usuario.focus();
-    }
+  });
+  if (usuario.value == userValid.user && senha.value == userValid.senha) {
+    window.location.href = "../pages/perfilUsuario.html";
+    let token = Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2);
+    localStorage.setItem("token", token);
+    localStorage.setItem("userLogado", JSON.stringify(userValid));
+  } else {
+    userLabel.setAttribute("style", "color: red");
+    usuario.setAttribute("style", "border-color: red");
+    senha.setAttribute("style", "border-color: red");
+    passwordLabel.setAttribute("style", "color: red");
+    msgError.setAttribute("style", "display: block");
+    msgError.innerHTML = "Usuário ou senha incorretos";
+    usuario.focus();
+  }
 }
